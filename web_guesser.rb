9b3a216cb2ff0fc6c0fd608b7fc4ff0c84469bb4
Lynn Chin <lynn.chin@hotmail.com>
2016-09-22 21:44:48 +1000

    I4: Add colorizing on output depends on input and guess numbers

--- web_guesser.rb
+++ web_guesser.rb
@@ -7,19 +7,28 @@ def check_guess(guess)
  guess_number = guess.to_i
  if SECRET_NUMBER > guess_number + 5
     message = "Way too low!"
+    backgroundColor = 'red'
  elsif SECRET_NUMBER < guess_number - 5
     message = "Way too high!"
+    backgroundColor = 'red'
  elsif SECRET_NUMBER > guess_number
    message = "Too low!"
+     backgroundColor = '#F27989'
  elsif SECRET_NUMBER < guess_number
    message = "Too high!"
+     backgroundColor = '#F27989'
  else
    message = "You got it right!"
+     backgroundColor = 'green'
  end
+ return message, SECRET_NUMBER, backgroundColor
 end
 
 get '/' do
  guess = params['guess']
- message = check_guess(guess)
- erb :index, :locals => {:number => SECRET_NUMBER, :message => message }
+ message = (check_guess(guess))[0]
+ number = check_guess(guess)[1]
+ backgroundColor = (check_guess(guess))[2]
+ 
+ erb :index, :locals => {:number => number, :message => message, :backgroundColor => backgroundColor }
 end
